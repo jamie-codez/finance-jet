f1d835887a2054f3567e0acaa700419d4e564cea
jamie-codez <cruiseomondi90@gmail.com>
2024-02-27 16:09:22 +0300

    Added finance section

--- app/src/main/java/com/jamie/jet/MainActivity.kt
+++ app/src/main/java/com/jamie/jet/MainActivity.kt
@@ -71,7 +71,7 @@ fun HomeScreen() {
             WalletSection()
             CardSection()
             Spacer(modifier = Modifier.height(16.dp))
-//            FinanceSection()
+            FinanceSection()
 //            CurrenciesSection()
         }
     }
